68a586cb0b19589d7e87b5c5fb5511cb25290bd7
dezmunt1 <dezmunt1@gmail.com>
2022-11-09 21:29:25 +0300

    feat: github ci/cd

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,7 @@ import * as dotenv from 'dotenv';
 import { join } from 'path';
 
 dotenv.config({
-  path: join(
-    __dirname,
-    '..',
-    `.env.${
-      process.env.NODE_ENV === 'production' ? 'production' : 'development'
-    }`,
-  ),
+  path: join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
 });
 
 import './app';
