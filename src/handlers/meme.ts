9e32b902dd022225cf734f5a1301135d3646dfb1
dezmunt1 <dezmunt1@gmail.com>
2022-11-12 01:43:39 +0300

    fix: meme request problem; add local session

--- src/handlers/meme.ts
+++ src/handlers/meme.ts
@@ -3,6 +3,14 @@ import axios from 'axios';
 
 type MemeContext = Context & { message: { text: string } };
 
+declare module 'telegraf' {
+  interface Context {
+    session: {
+      isMemeLoading?: boolean;
+    };
+  }
+}
+
 const MoreButton = Markup.inlineKeyboard([
   Markup.button.callback('MOAR', 'next_meme'),
 ]);
@@ -10,26 +18,31 @@ const isGif = (url: string) => /\.gif$/.test(url);
 
 async function nextMeme(ctx: MemeContext, subReddit?: string) {
   try {
+    ctx.session.isMemeLoading = true;
     const { data } = await axios.get(
       `${process.env.MEME_API}${subReddit ?? ''}`,
     );
 
-    const message = `_${data.title}_\n*Subreddit:* __${data.subreddit}__`;
+    const message = `<i>${data.title}</i>\n<b>Subreddit:</b> <code>${data.subreddit}</code>`;
 
     if (isGif(data.url)) {
       return await ctx.replyWithAnimation(data.url, {
         caption: message,
-        parse_mode: 'Markdown',
+        parse_mode: 'HTML',
         ...MoreButton,
       });
     }
     ctx.replyWithPhoto(data.url, {
       caption: message,
-      parse_mode: 'Markdown',
+      parse_mode: 'HTML',
       ...MoreButton,
     });
   } catch (e) {
-    ctx.reply('Is not valid subreddit name');
+    console.log((e as any)?.response);
+
+    ctx.reply('Failure! Try again..');
+  } finally {
+    ctx.session.isMemeLoading = false;
   }
 }
 
@@ -39,6 +52,11 @@ export const memeHandler = async (ctx: MemeContext) => {
 };
 
 export const nextMemeAction = async (ctx: Context) => {
-  nextMeme(ctx as MemeContext);
+  if (ctx.session.isMemeLoading && ctx.callbackQuery) {
+    await ctx.answerCbQuery('Request still in progress');
+    return;
+  }
+
+  await nextMeme(ctx as MemeContext);
   ctx.answerCbQuery();
 };
